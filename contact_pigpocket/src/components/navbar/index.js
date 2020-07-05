69c182e2c5a078b5a989cfe44e85b205526bb93f
tfiosecmtv <tfiosecmtv@gmail.com>
2020-07-05 11:13:54 +0500

    observer-observable added; state in separate file

--- contact_pigpocket/src/components/navbar/index.js
+++ contact_pigpocket/src/components/navbar/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {observer} from "mobx-react";
 import {makeStyles} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import {Link} from "react-router-dom";
+import { observer } from "mobx-react"
+import {appState, handleChange} from "./state-mobx"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,21 +14,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default observer(() => {
-  const classes = useStyles();
-  // Declare a new state variable, which we'll call "value"
-  const [value, setValue] = React.useState(0);
+export default observer(() => { //mobx observer HoC / decorator subscribes React components automatically to any observables that are used during render. 
+  const classes = useStyles(); //lets use state without creating classes
 
-  const handleChange = (newValue) => {
-    //console.log(newValue);
-    setValue(newValue);
-  };
+  // Declare a new state variable, which we'll call "value"
+  // const [value, setValue] = React.useState(0); //turning value into observable component
+
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Tabs
-          value={value}
+          value={appState.count}
           onChange={handleChange}
           aria-label="simple tabs example"
         >
@@ -39,10 +37,10 @@ export default observer(() => {
             to={"/contact"}
           />
           <Tab label="FAQ" value={"/faq"} component={Link} to={"/faq"} />
-          <p>You clicked {value} times</p>
-          <button onClick={() => setValue(value + 1)}>
+          <p>You clicked {appState.count} times</p>
+          <button onClick={() => handleChange()}> 
     Click me
-  </button>
+  </button> //incrementing the value everytime the button is clicked
         </Tabs>
       </AppBar>
     </div>
